6e20648daeeca0e3966d45a8825a0270bdaf50e1
GKenethCG <geicagui489@gmail.com>
2026-09-15 23:50:58 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Interacciones sencillas para el proyecto académico.
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', () => {
-    const target = document.querySelector(link.getAttribute('href'));
-    if (target) target.scrollIntoView({behavior:'smooth'});
-  });
-});
